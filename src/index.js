f30f05df912b54d3233fc19293b95fa060f09e34
TrinhChung <chungtrinh2k2@gmail.com>
2022-10-23 22:58:10 +0700

    interpolation Newton

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { falsePositionMethod } from "./method/iterativeMethod.js";
 import { luMethod } from "./method/matrix.js";
 import { seidelMethod } from "./method/iterativeMatrix.js";
-import { interpolationNewtonCalculate } from "./method/interpolation.js";
+import { interpolationNewtonMethod } from "./method/interpolation.js";
 
-interpolationNewtonCalculate();
+interpolationNewtonMethod();
--- src/method/interpolation.js
+++ src/method/interpolation.js
@@ -11,7 +11,7 @@ const lkMethod = (x, arr, n) => {
   return result;
 };
 
-const interpolationNewtonMethod = (arr, x) => {
+const interpolationLagrangeMethod = (arr, x) => {
   let result = 0;
   for (let i = 0; i < arr.length; i++) {
     result =
@@ -20,7 +20,7 @@ const interpolationNewtonMethod = (arr, x) => {
   return result;
 };
 
-export const interpolationNewtonCalculate = () => {
+export const interpolationLagrangeCalculate = () => {
   const f = math.parse("log(x)");
   const arr = [
     [9, 2.19722458],
@@ -29,7 +29,43 @@ export const interpolationNewtonCalculate = () => {
     [12, 2.48490665],
   ];
   const x = { x: 9.2 };
-  const p = interpolationNewtonMethod(arr, x.x);
+  const p = interpolationLagrangeMethod(arr, x.x);
   console.log("p(x)= " + p);
   console.log("Sai số tuyệt đối là " + math.abs(f.evaluate(x) - p));
+};
+
+const anCalculate = (arr, n) => {
+  if (n === 0) return arr[0][1];
+  let m = 1.0;
+  for (let i = 0; i < n; i++) {
+    m = m * (arr[n][0] - arr[i][0]);
+  }
+  return (arr[n][1] - interpolationNewtonCalculate(arr, arr[n][0], n - 1)) / m;
+};
+
+const interpolationNewtonCalculate = (arr, x, n) => {
+  if (n === 0) return anCalculate(arr, 0);
+  let t = 1;
+  for (let i = 0; i < n; i++) {
+    t *= x - arr[i][0];
+  }
+  return interpolationNewtonCalculate(arr, x, n - 1) + anCalculate(arr, n) * t;
+};
+
+export const interpolationNewtonMethod = () => {
+  const f = math.parse("log(x)");
+  const arr = [
+    [9, 2.19722458],
+    [9.5, 2.2512918],
+    [11, 2.39789527],
+    [12, 2.48490665],
+    [13, 2.564949357],
+    [14, 2.63905733],
+    [15, 2.708050201],
+  ];
+  const x = { x: 13 };
+  const pn = interpolationNewtonCalculate(arr, x.x, arr.length - 1);
+
+  console.log("Pn(x)= " + pn);
+  console.log("Sai số tuyệt đối là: " + math.abs(f.evaluate(x) - pn));
 };
